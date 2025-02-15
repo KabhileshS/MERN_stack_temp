9af3e9e5285de3e029fb772cef7105bcbbae33a3
KabhileshS <kabhilesh14@gmail.com>
2025-02-15 10:16:26 +0530

    Day 6 FN An

---
+++ JavaScript/es6.js
@@ -0,0 +1,10 @@
+//arrow function
+
+var testArrowFunction = () => {
+    console.log("This is test arrow function")
+}
+testArrowFunction()
+
+// 1. variables refer to variables
+// 2. arrow function => 
+    // var functionName=()=>{}
---
+++ Tasks from day 5/function.js
@@ -0,0 +1,6 @@
+//tasks - to practice 4 types of functions with or without parameters
+
+function testFunction(){
+    console.log("this is test function")
+}
+testFunction()
