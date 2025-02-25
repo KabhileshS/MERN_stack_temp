3c8c8d5817cbbbe9911ef695a8bdd3224234a89f
KabhileshS <kabhilesh14@gmail.com>
2025-02-25 13:20:19 +0530

    Day 13 FN

--- Backend1/index.js
+++ Backend1/index.js
@@ -1,11 +1,41 @@
 const express = require("express");
-
+const mdb = require("mongoose");
+const dotenv = require("dotenv");
+const Signup = require("./models/signupSchema")
 const app = express();
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to Backend Server Hehe </h1>")
-})
-
+app.use(express.json())
 const PORT = 3001;
+dotenv.config();
+
+mdb.connect("mongodb+srv://kabhilesh14:kabhilesh14@cluster0.psjy1hg.mongodb.net/sjit")   //if doesnt connect use this inside paranthesis- "mongodb://127.0.0.1:27017/sjit" which is local compass
+  .then(() => {
+    console.log("DB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("Check your Connection String", err);
+  });
+
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to Backend Server Hehekeke </h1>");
+});
+
+app.post("/signup",(req,res)=>{
+    try{
+        const {firstName,lastName,email,password,phoneNumber}=req.body
+        const newSignup= new Signup({
+            firstName:firstName,
+            lastName:lastName,
+            email:email,
+            password:password,
+            phoneNumber:phoneNumber,
+        })
+        newSignup.save()
+        console.log("Signup Successful")
+        res.status(201).json({message:"Signup Successful",isSignup:true})
+    }
+    catch(error){
+        res.status(201).json({message:"Signup UnSuccessful",isSignup:false})
+    }
+})
 
 app.listen(PORT, () => console.log("Server Started Successfully"));
